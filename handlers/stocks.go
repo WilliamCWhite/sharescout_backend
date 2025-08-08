857b1017065dd35bba8870fa9e1ec4a7b16d4e33
WilliamCWhite <williamcwhite323@gmail.com>
2025-08-08 15:01:16 -0400

    Refactored to fit frontend type requirements better

--- handlers/stocks.go
+++ handlers/stocks.go
@@ -13,14 +13,15 @@ import (
 	"github.com/piquette/finance-go/datetime"
 )
 
-type ChartInterval struct {
+type RequestInterval struct {
 	StartDate time.Time `json:"start_date"`
 	EndDate   time.Time `json:"end_date"`
 }
 
-type ResponseBar struct {
-	Close float64 `json:"close_price"`
-	Date time.Time `json:"date"`
+// Contains float value and UNIX timestamp / 1000
+type DataPoint struct {
+	Value float64 `json:"value"`
+	Timestamp int `json:"time"`
 }
 
 // Simply for ensuring that the backend is receiving requests
@@ -35,32 +36,32 @@ func StocksHandler(w http.ResponseWriter, r *http.Request) {
 	ticker := vars["ticker"]
 	fmt.Printf("Processing request with ticker %v\n", ticker)
 
-	var chartInterval ChartInterval
-	err := json.NewDecoder(r.Body).Decode(&chartInterval)
+	var reqInterval RequestInterval
+	err := json.NewDecoder(r.Body).Decode(&reqInterval)
 	if err != nil {
 		fmt.Printf("Decoding error: %v\n", err)
 		http.Error(w, "Invalid JSON in request", http.StatusBadRequest)
 		return
 	}
 
-	interval := lib.DetermineInterval(chartInterval.StartDate, chartInterval.EndDate)
+	interval := lib.DetermineInterval(reqInterval.StartDate, reqInterval.EndDate)
 
 	params := &chart.Params{
 		Symbol: ticker,
-		Start: datetime.New(&chartInterval.StartDate),
-		End: datetime.New(&chartInterval.EndDate),
+		Start: datetime.New(&reqInterval.StartDate),
+		End: datetime.New(&reqInterval.EndDate),
 		Interval: interval,
 	}
 
 	iter := chart.Get(params)
 
-	jsonRes := []ResponseBar{}
+	jsonRes := []DataPoint{}
 	for iter.Next() {
 		bar := iter.Bar()
 		close_price, _ := bar.Close.Float64()
-		rBar := ResponseBar{
-			Close: close_price,
-			Date: time.Unix(int64(bar.Timestamp), 0).UTC(),
+		rBar := DataPoint{
+			Value: close_price,
+			Timestamp: bar.Timestamp, //bar.Timestamp happens to give the exact data format the frontend wants
 		}
 		jsonRes = append(jsonRes, rBar)
 	}
